11a55cc573bbdcc256fc1efb803a15485df2c1bd
progressEdd <progressEdd@gmail.com>
2020-12-11 11:23:51 -0600

    most recent ER model Updates

--- sql_demos.sql
+++ sql_demos.sql
@@ -1,3 +1,4 @@
+-- event attendance
 SELECT first_name, last_name, email, college, category, attendance.person_id, attendance.event_id, attendance.role, event.event_type, event.event_date 
 FROM person  
 JOIN attendance  
@@ -6,21 +7,17 @@ JOIN event
 ON attendance.event_id = event.id 
 WHERE event.id = 222; 
 
-SELECT donor.person_id, first_name, last_name, donor.organization, donations.date, donations.item_name, donations.item_desc, donations.value  
+-- donor
+SELECT donor.person_id, first_name, last_name, donor.organization, donation.date, donation.item_name, donation.item_desc, donation.value  
 FROM person  
 JOIN donor  
 ON person.id = donor.person_id 
-JOIN donations 
-ON donations.donor_id = donor.id 
+JOIN donation 
+ON donation.donor_id = donor.id 
 WHERE donor.id = 1010; 
 
-SELECT number_attendees, person.first_name, person.last_name 
-FROM person
-FULL JOIN birthday
-ON person.id = birthday.event_person_id
-WHERE birthday.id = 1; 
-
-SELECT first_name, last_name, email, college, category, attendance.person_id, attendance.event_id, attendance.role, event.event_type, event.event_date, birthday.number_attendees 
+-- birthday
+SELECT first_name, last_name, email, college, category, attendance.person_id, attendance.event_id, attendance.role, event.event_type, event.event_date
 FROM person  
 JOIN attendance  
 ON person.id = attendance.person_id  
@@ -29,3 +26,13 @@ ON attendance.event_id = event.id
 JOIN birthday
 ON event.id = birthday.event_id
 WHERE number_attendees  = 5; 
+
+-- volunteer
+SELECT person_id, first_name, last_name, email, college, category, employee_type, employee.postion 
+FROM person
+JOIN employee 
+ON person.id = employee.person_id
+JOIN timesheet
+ON employee_person_id = timesheet.employee_person_id
+GROUP BY person_id
+HAVING employee.postion = "volunteer";
